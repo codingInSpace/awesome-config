060ed43eed5e6f0c57d921b25307311014431c25
codingInSpace <jonathan.grangien@gmail.com>
2015-12-27 02:13:11 +0100

    Apply multicolor theme and customize

--- themes/multicolor/theme.lua
+++ themes/multicolor/theme.lua
@@ -10,7 +10,7 @@
 theme                               = {}
 
 theme.confdir                       = os.getenv("HOME") .. "/.config/awesome/themes/multicolor"
-theme.wallpaper                     = theme.confdir .. "/wall.png"
+theme.wallpaper                     = theme.confdir .. "/wall2.jpg"
 
 theme.font                          = "Terminus 8"
 --theme.taglist_font                =
